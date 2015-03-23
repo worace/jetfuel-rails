09fb1a10ced8cbc1983a441a32e33b6faa414a9a
Nathan Owsiany <nowsiany@gmail.com>
2015-03-23 13:57:01 -0600

    Hound: add in missing space

--- app/workers/title_worker.rb
+++ app/workers/title_worker.rb
@@ -4,7 +4,7 @@ class TitleWorker
   def perform(url_id)
     url = Url.find_by(id: url_id)
     doc = Nokogiri::HTML(HTTParty.get(url.long))
-    title = doc.css("title").first.child.text.tr("\n"," ").split.join(" ")
+    title = doc.css("title").first.child.text.tr("\n", " ").split.join(" ")
     url.update_column(:title, title)
   end
 end
